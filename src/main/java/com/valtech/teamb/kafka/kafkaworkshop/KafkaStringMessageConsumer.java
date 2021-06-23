8a493e9df1e7c48b375d3ad2e01d507761f7daea
Boris Fresow <boris.fresow@valtech.com>
2021-06-23 08:45:09 +0200

    Work package #1

--- src/main/java/com/valtech/teamb/kafka/kafkaworkshop/KafkaStringMessageConsumer.java
+++ src/main/java/com/valtech/teamb/kafka/kafkaworkshop/KafkaStringMessageConsumer.java
@@ -2,7 +2,6 @@ package com.valtech.teamb.kafka.kafkaworkshop;
 
 import lombok.Getter;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
